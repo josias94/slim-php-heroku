dd0d25a6f100e2f2f7701c6529a46add3d036777
josias94 <josiasrivola@gmail.com>
2021-03-23 20:17:12 -0300

    Update index.php

--- app/Clase2/index.php
+++ app/Clase2/index.php
@@ -6,8 +6,8 @@ Mostrar por pantalla las primeras 4 potencias de los
 números del uno 1 al 4 (hacer una función
 que las calcule invocando la función pow).
 */
-for ($i=0; $i < 5; $i++) { 
-    for ($j=0; $j < 5; $j++) { 
+for ($i=1; $i < 5; $i++) { 
+    for ($j=1; $j < 5; $j++) { 
         echo($i);
         echo("E");
         echo("$j: ");
