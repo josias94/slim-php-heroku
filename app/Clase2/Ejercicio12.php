6284cb5fab8c3d13bceb6274e8b83843adf2029b
josias94 <josiasrivola@gmail.com>
2021-03-23 21:13:01 -0300

    Update Ejercicio12.php

--- app/Clase2/Ejercicio12.php
+++ app/Clase2/Ejercicio12.php
@@ -7,12 +7,13 @@ de caracteres y que invierta el orden
 de las letras del Array.
 Ejemplo: Se recibe la palabra “HOLA” y luego queda “ALOH”.
 */
-function($array)
-{
+echo Invertir("1 Amo la pacifica paloma 2");
 
+
+function Invertir($array)
+{
     $invertido = array_reverse($array);
     echo $array."<br>";
-    echo $invertido;
-    
+    echo $invertido;  
 }
 ?>
