21f9d7cea7c9b98652e7eeb859cad761b9a8ba5b
josias94 <josiasrivola@gmail.com>
2021-03-26 18:22:53 -0300

    Update Ejercicio16.php

--- app/Clase2/Ejercicio16.php
+++ app/Clase2/Ejercicio16.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+Rivola Josias
+Aplicación Nº 16 (Rectangulo - Punto)
+Codificar las clases Punto y Rectangulo.
+La clase Punto ha de tener dos atributos privados con acceso de sólo lectura (sólo con
+getters), que serán las coordenadas del punto. Su constructor recibirá las coordenadas del
+punto.
+La clase Rectangulo tiene los atributos privados de tipo Punto _vertice1, _vertice2, _vertice3
+y _vertice4 (que corresponden a los cuatro vértices del rectángulo).
+La base de todos los rectángulos de esta clase será siempre horizontal. Por lo tanto, debe tener
+un constructor para construir el rectángulo por medio de los vértices 1 y 3.
+Los atributos ladoUno, ladoDos, área y perímetro se deberán inicializar una vez construido el
+rectángulo.
+Desarrollar una aplicación que muestre todos los datos del rectángulo y lo dibuje en la página.
+*/
+
 class Punto{
     private $_x;
     private $_y;
