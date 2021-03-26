4ec6686c90bcaef3948db622428df152ac82786d
josias94 <josiasrivola@gmail.com>
2021-03-26 12:16:41 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,47 +1,47 @@
 <?php
-include_once("PHP/Objetos/Persona.php");
-include_once("PHP/Clases/Archivos.php");
+// include_once("PHP/Objetos/Persona.php");
+// include_once("PHP/Clases/Archivos.php");
 
-//$var = $_GET["opcion"];
-$var = 9;
-$file_name = "Archivos/ListaPersonas.txt";
+// //$var = $_GET["opcion"];
+// $var = 9;
+// $file_name = "Archivos/ListaPersonas.txt";
 
-echo "Agregar a la url /Clase2/EjercicioN.php";
+// echo "Agregar a la url /Clase2/EjercicioN.php";
 
-if($var == 0)
-{
-    $arrayHarcodeado = LeerArchivo("Archivos/PersonasHarcodeadas.txt");
-    EscribirArchivo($file_name, $arrayHarcodeado);
-}
-else if($var == 1)
-{        
-    $persona = Persona("Josias", "Rivola", "26", "Masculino");
-    EscribirArchivo($file_name, $persona);
-}
-else if ($var == 2)
-{
-    $array = LeerArchivo($file_name);
-    foreach ($array as $key => $value) {
-        MostrarKeyValue($value);
-        printf("////////////////////////////////////<br/>");
-    }
-}
+// if($var == 0)
+// {
+//     $arrayHarcodeado = LeerArchivo("Archivos/PersonasHarcodeadas.txt");
+//     EscribirArchivo($file_name, $arrayHarcodeado);
+// }
+// else if($var == 1)
+// {        
+//     $persona = Persona("Josias", "Rivola", "26", "Masculino");
+//     EscribirArchivo($file_name, $persona);
+// }
+// else if ($var == 2)
+// {
+//     $array = LeerArchivo($file_name);
+//     foreach ($array as $key => $value) {
+//         MostrarKeyValue($value);
+//         printf("////////////////////////////////////<br/>");
+//     }
+// }
 
-function Algo()
-{
-    printf("LLEGUE");
-}
+// function Algo()
+// {
+//     printf("LLEGUE");
+// }
 
-function MostrarValue($obj){
-    foreach ($obj as $value) {
-        printf("$value<br/>");
-    }
-}
+// function MostrarValue($obj){
+//     foreach ($obj as $value) {
+//         printf("$value<br/>");
+//     }
+// }
 
-function MostrarKeyValue($obj){
-    foreach ($obj as $key => $value) {
-        printf("$key = $value<br/>");
-    }
-}
+// function MostrarKeyValue($obj){
+//     foreach ($obj as $key => $value) {
+//         printf("$key = $value<br/>");
+//     }
+// }
 
 ?>
