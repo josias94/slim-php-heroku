e2fc9939b6b742b343fe243336dff3c594ac923f
josias94 <josiasrivola@gmail.com>
2021-05-16 14:34:34 -0300

    asd

--- app/Comanda/index.php
+++ app/Comanda/index.php
@@ -1,3 +1,32 @@
 <?php
-echo "asd";
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once '../vendor/autoload.php';
+require_once './clases/AccesoDatos.php';
+require_once './clases/cdApi.php';
+
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+
+$app = new \Slim\App(["settings" => $config]);
+
+/*LLAMADA A METODOS DE INSTANCIA DE UNA CLASE*/
+$app->group('/cd', function () {
+ 
+  $this->get('/', \cdApi::class . ':traerTodos');
+ 
+  $this->get('/{id}', \cdApi::class . ':traerUno');
+
+  $this->post('/', \cdApi::class . ':CargarUno');
+
+  $this->delete('/', \cdApi::class . ':BorrarUno');
+
+  $this->put('/', \cdApi::class . ':ModificarUno');
+     
+});
+
+$app->run();
 ?>
